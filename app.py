397c37582a648c525515cb8d9b11e068514a3ce6
sambavsphere@gmail.com <sambavsphere@gmail.com>
2017-02-13 10:49:01 +0530

    2017-FEB-13

---
+++ app.py
@@ -0,0 +1,60 @@
+'''
+import flask
+
+print flask.__file__
+'''
+'''
+from flask import Flask
+app = Flask(__name__)
+import json
+
+def home1():
+	return "home1"
+@app.route("/",methods=['GET','POST'])
+def home():
+	result = home1()
+	return result+"hello world"
+@app.route('/users')
+def users():
+
+	return json.dumps(['user1','user2','user3'])
+
+if __name__ == "__main__":
+	app.run(port=8889, debug=True)
+
+'''
+''''
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+#app.config.from_object(__name__)
+app.config.from_pyfile('config_file.cfg')
+
+
+if __name__ == "__main__":
+	app.run(port=8889)
+	'''
+from flask import Flask, render_template
+import pandas as pd
+
+
+app = Flask(__name__,static_folder="static1")
+
+@app.route("/users")
+def users():
+	data = pd.read_csv('users.csv')
+	columns = data.columns.tolist()
+	users = data.values.tolist()
+	return render_template('home.html',users=users,columns=columns)
+
+@app.route("/home")
+def home():
+
+	return render_template('home.html')
+	return data
+	return "<h1>home file</h1>"
+if __name__ == "__main__":
+	app.run(port=8889,debug=True)
---
+++ call_api.py
@@ -0,0 +1,4 @@
+import requests
+import pdb
+resp = requests.get('http://localhost:8889/users')
+pdb.set_trace()
